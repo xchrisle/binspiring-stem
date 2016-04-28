2829f4ecaab5bf15e65594308173df237f804749
xchrisle <chrisle1994@gmail.com>
2016-04-28 17:02:24 +1000

    add template and example

--- binspiring-stem/config/routes.rb
+++ binspiring-stem/config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get '/template' => "code#template"
+  get '/ice-cream' => "code#ice-cream"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
