6c2cbf5fbd03232a0ee7d0c74c96cbe7773c062d
xchrisle <chrisle1994@gmail.com>
2016-06-27 04:15:35 +1000

    add the internet of html and css

--- binspiring-stem/config/routes.rb
+++ binspiring-stem/config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get '/template' => "code#template"
+  get '/template2' => "code#template2"
+  get '/template3' => "code#template3"
   get '/ice-cream' => "code#ice-cream"
+  get '/internet' => "code#internet"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
